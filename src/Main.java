f5c0f0fcbca229ed3c5cb259c5e06813db24b5a6
Tomas De Paulo <tomas.depaulo@davinci.edu.ar>
2023-05-22 10:55:45 -0300

    feat: basic teams

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import Tournament.Team;
 
 public class Main {
     public static void main(String[] args) {
-       
+       Team TumiTeam = new Team();
+       TumiTeam.getPlayers().forEach(player -> {
+           System.out.println(player.getName());
+       });
     }
 }
